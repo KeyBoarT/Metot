98b5f60f189aa817a742ca53933ec2e2a5693893
Mehmet Sezer <mehmetszr05@gmail.com>
2022-04-30 11:45:46 +0300

    Proje dosyası ekle.

---
+++ Metot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Toplamak istediğiniz birinci sayıyı giriniz : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Toplamak istediğiniz ikinci sayıyı giriniz : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int toplam = Topla(x, y);
+            Console.WriteLine("Toplam = " + toplam);
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
